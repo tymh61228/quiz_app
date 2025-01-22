4cbcd0837c7f10f400e8066a165e7dde8ef8431e
tymh61228 <tymh61228@gmail.com>
2025-01-22 18:00:08 +0900

    認証まわり

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,9 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__ . '/auth.php';
 
-
-Route::get('/admin/top', function () {
-    return view('admin.top');
-})->name('admin.top');
+Route::middleware('auth')->group(function () {
+    //管理画面トップページ
+    Route::get('/admin/top', function () {
+        return view('admin.top');
+    })->name('admin.top');
+});
