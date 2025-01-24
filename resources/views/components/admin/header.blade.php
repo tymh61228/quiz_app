a75f8d2eaf5e4ff27e01d1d9a6b240cae17f4d90
tymh61228 <tymh61228@gmail.com>
2025-01-24 17:14:13 +0900

    微修正

--- resources/views/components/admin/header.blade.php
+++ resources/views/components/admin/header.blade.php
@@ -13,7 +13,7 @@
             @csrf
             <button :href="route('logout')" onclick="event.preventDefault();
             this.closest('form').submit();"
-                class="inline-flex items-center text-white bg-gray-700 border-0 py-1 px-3 focus:outline-none hover:bg-gray-800 rounded text-base mt-4 md:mt-0">ログアウト
+                class="inline-flex items-center text-white bg-gray-700 border-0 py-1 px-3 focus:outline-none hover:bg-gray-800 rounded text-base mt-4 md:mt-0">Logout
                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
                     stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
                     <path d="M5 12h14M12 5l7 7-7 7"></path>
