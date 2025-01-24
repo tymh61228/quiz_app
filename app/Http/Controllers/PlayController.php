588d90f473abdc3c85928abe18caf8b2c00b65e9
tymh61228 <tymh61228@gmail.com>
2025-01-24 12:55:31 +0900

    クイズスタート画面実装

--- app/Http/Controllers/PlayController.php
+++ app/Http/Controllers/PlayController.php
@@ -7,11 +7,26 @@ use Illuminate\Http\Request;
 
 class PlayController extends Controller
 {
+    /**
+     * プレイ画面トップページ
+     */
     public function top()
     {
         $categories = Category::all();
         return view('play.top', [
             'categories' => $categories,
+        ]);
+    }
+
+    /**
+     * クイズスタート画面表示
+     */
+    public function categories(Request $request, int $categoryId)
+    {
+        $category = Category::withCount('quizzes')->findOrFail($categoryId);
+        return view('play.start', [
+            'category' => $category,
+            'quizzesCount' => $category->quizzes_count,
         ]);
     }
 }
