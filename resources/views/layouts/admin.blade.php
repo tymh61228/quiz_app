1654e974e39c6e795b8d01aa070254d48fb5ae1e
tymh61228 <tymh61228@gmail.com>
2025-01-29 11:35:29 +0900

    ファビコン修正

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,7 +1,10 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
+@section('title', 'Quiz-kun')
+
 <head>
+    <link rel="shortcut icon" href="{{ asset('/favicon.ico') }}">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
